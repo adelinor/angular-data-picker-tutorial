400bd094b70ab37cdcc61b9260f5e7641728714e
Adelino Rodrigues <adelinor@gmail.com>
2013-10-11 04:51:47 +0100

    Use templateUrl in directive

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,6 @@ var app = angular.module("data-picker-tutorial", []);
 app.directive("datapicker", function() {
 	return {
 		restrict: "E",
-		template: "<div>This is the datapicker"
+		templateUrl: "datapicker/view.html"
 	}
 })
