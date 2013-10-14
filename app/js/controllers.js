62ded241ee54af163283b2c6988af069c94d4604
Adelino Rodrigues <adelinor@gmail.com>
2013-10-14 22:54:28 +0100

    add edit button against names

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,7 +14,22 @@ function PeopleCtrl($scope, $http) {
 	$scope.people = [];
 
 	$scope.addPerson = function() {
-		$scope.people.push( {name:$scope.personNameNG} );
+		var pos = $scope.people.length;
+		$scope.people.push(
+			{
+				id: pos,
+				name:$scope.personNameNG
+			} );
 		$scope.personNameNG = '';
 	};
+
+	$scope.editById = function(id) {
+		alert('You want to edit person with ID=' + id);
+		/* This needs to somehow refresh the model for the Edit form
+		 * which should then change the title, label, prepopulate the form fields
+		 * and setup the label on the button
+		 * the callback would need to behave in a different way: the entire object
+		 * needs to be passed, if no ID, create it. Otherwise update existing.
+		 */
+	}
 }
