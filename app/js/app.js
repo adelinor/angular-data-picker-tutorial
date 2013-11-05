f25ff1d15531177cdbb0669c6c3790dde51ffcd1
Adelino Rodrigues <adelinor@gmail.com>
2013-11-05 22:09:49 +0000

    Define very basic behaviour in directive

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,23 @@ var app = angular.module("data-picker-tutorial", []);
 app.directive("datapicker", function() {
 	return {
 		restrict: "E",
-		templateUrl: "datapicker/view.html"
+		templateUrl: "datapicker/datapicker.html"
+		//From directives talking to controllers video
+/*
+		,
+		controller: function() {
+			return function(scope, element, attrs) {
+				scope.message = "I am working on it";
+//				element.bind("mouseenter", function() {
+//					alert("I am doing the job");
+//				})
+			}
+		}
+*/
+		,
+		//From angular-app/client/src/app/admin/users/admin-users-edit.js
+		link: function(scope, el, attrs, ctrl) {
+			scope.message = "I am working on it";
+		}
 	}
 })
