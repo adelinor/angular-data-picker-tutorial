179fca881bb2ea60b1ffe56dc08712dd37610c5d
Adelino Rodrigues <adelinor@gmail.com>
2014-01-26 21:47:22 +0000

    Step 3 implementation start

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,18 +12,48 @@ function OrgsListCtrl($scope, $http) {
 
 function PeopleCtrl($scope, $http) {
 	$scope.people = [];
+	$scope.person = {};
 
 	$scope.addPerson = function() {
-		var pos = $scope.people.length;
-		$scope.people.push(
-			{
-				id: pos,
-				name:$scope.personNameNG
-			} );
-		$scope.personNameNG = '';
+		if (typeof $scope.person.id == 'undefined' ) {
+			var pos = $scope.people.length;
+
+			//Assign id
+			$scope.person.id = pos;
+
+			$scope.people.push( $scope.person );
+
+		} else {
+            //Replace object in list
+            var id = $scope.person.id;
+            var pList = $scope.people;
+            var i=0, len=pList.length, notReplaced = true;
+
+            for (; notReplaced && i < len; i++) {
+                if (pList[i].id === id) {
+                    pList[i] = $scope.person;
+                    notReplaced = false;
+                }
+            }
+        }
+
+		//Reset form data via the bound object
+		$scope.person = {};
 	};
 
 	$scope.editById = function(id) {
-		alert('You want to edit person with ID=' + id);
-	}
+        var pList = $scope.people;
+        var i=0, len=pList.length;
+        var p = null, item = null;
+
+        for (; p == null && i < len; i++) {
+            item = pList[i];
+            if (item.id === id) {
+                //p = item;
+                //We edit a copy
+                p = angular.copy(item);
+            }
+        }
+        $scope.person = p;
+    }
 };
